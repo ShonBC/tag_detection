732aeb9c69a546354eadad87dd260f3b5905d230
ShonBC <cortesshon@gmail.com>
2022-02-15 15:53:14 -0800

    Script to record video for camera calibration

---
+++ calibration/rec_vid.py
@@ -0,0 +1,24 @@
+from tkinter import Frame
+import cv2
+from torch import frac
+
+cap = cv2.VideoCapture(0)
+
+fourcc  = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter('out.mp4', fourcc, 20, (640, 480))
+
+while True:
+    ret, frame = cap.read()
+
+    out.write(frame)
+
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+
+out.release()
+
+cv2.destroyAllWindows()
