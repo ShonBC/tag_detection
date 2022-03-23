d53badcf0b0248f11704056c66e058b8be5ea2f1
ShonBC <cortesshon@gmail.com>
2022-03-22 21:53:14 -0700

    Adjusted to work on live video

--- calibration/vidcalibration.py
+++ calibration/vidcalibration.py
@@ -6,18 +6,19 @@ import yaml
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # Calibration chessboard size
-chess_width = 7
-chess_height = 6
+chess_width = 6
+chess_height = 5
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((chess_width * chess_height, 3), np.float32)
+objp[:,:2] = np.mgrid[0:chess_width, 0:chess_height].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
 
-cap = cv.VideoCapture('out.mp4')
+# cap = cv.VideoCapture('out.mp4')
+cap = cv.VideoCapture(0)
 
 if(cap.isOpened() == False):
     print('Error opening video stream of file')
@@ -39,30 +40,29 @@ while True:
             cv.drawChessboardCorners(frame, (chess_width, chess_height), corners2, found)
             cv.imshow('frame', frame)
 
+            height, width = gray.shape[:2]
+            # Camera Calibration
+            ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, (width, height), None, None)
+            print('Cam Calibration')
+
+            # Refine Camera Matrix
+            ref_cam_mtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (width, height), 1, (width, height))
+            print('Cam Matrix Refined')
+
+            # Transform the matrix and distortion coefficients to writable lists
+            data = {'camera_matrix': np.asarray(ref_cam_mtx).tolist(),
+                    'dist_coeff': np.asarray(dist).tolist(),
+                    'rvecs': np.asarray(rvecs).tolist(),
+                    'tvecs': np.asarray(tvecs).tolist()}
+
+            # Save calibration parameters to a yaml file
+            with open("calibration_matrix_test.yaml", "w") as f:
+                yaml.dump(data, f)
+
         if cv.waitKey(10) == 27:
             break
     else:
         break
 
-
 cv.destroyAllWindows()
 print('Destroyed all windows')
-
-height, width = gray.shape[:2]
-# Camera Calibration
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, (width, height), None, None)
-print('Cam Calibration')
-
-# Refine Camera Matrix
-ref_cam_mtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (width, height), 1, (width, height))
-print('Cam Matrix Refined')
-
-# Transform the matrix and distortion coefficients to writable lists
-data = {'camera_matrix': np.asarray(ref_cam_mtx).tolist(),
-        'dist_coeff': np.asarray(dist).tolist(),
-        'rvecs': np.asarray(rvecs).tolist(),
-        'tvecs': np.asarray(tvecs).tolist()}
-
-# Save calibration parameters to a yaml file
-with open("calibration_matrix.yaml", "w") as f:
-    yaml.dump(data, f)
