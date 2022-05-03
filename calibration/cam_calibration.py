0ea5b8d604854a8c58c6af1ca346cd1b806cf8f8
ShonBC <cortesshon@gmail.com>
2022-05-03 15:24:12 -0700

    Code cleanup

--- calibration/cam_calibration.py
+++ calibration/cam_calibration.py
@@ -3,6 +3,26 @@ import numpy as np
 import cv2 as cv
 import yaml
 
+
+def ReprojectionError(imgpoints, objpoints, mtx, dist, rvecs, tvecs):
+
+    total_err = 0
+    # view_err.resize(objpoints.size())
+
+    for i in range(len(objpoints)):
+        new_points, _ = cv.projectPoints(objpoints[i],
+                                         rvecs[i],
+                                         tvecs[i],
+                                         mtx,
+                                         dist)
+
+        err = cv.norm(imgpoints[i], new_points, cv.NORM_L2) / len(new_points)
+
+        total_err += err
+
+    return np.sqrt(total_err / len(objpoints))
+
+
 # Termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
@@ -11,12 +31,13 @@ chess_width = 7
 chess_height = 6
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((chess_width * chess_height, 3), np.float32)
+objp[:, :2] = np.mgrid[0:chess_width, 0:chess_height].T.reshape(-1, 2)
 
 # Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
+objpoints = []  # 3d point in real world space
+imgpoints = []  # 2d points in image plane.
+
 
 images = glob.glob('calibration/*.jpg')
 
@@ -25,27 +46,35 @@ for fname in images:
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(gray, (chess_height, chess_width), None)
+    found, corners = cv.findChessboardCorners(gray, (chess_height, chess_width), None)
 
     # If found, add object points, image points (after refining them)
-    if ret == True:
+    if found:
         objpoints.append(objp)
-        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-        imgpoints.append(corners)
+        corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        imgpoints.append(corners2)
 
         # Draw and display the corners
-        cv.drawChessboardCorners(img, (7,6), corners2, ret)
+        cv.drawChessboardCorners(img,
+                                 (chess_width, chess_height),
+                                 corners2,
+                                 found)
         cv.imshow('img', img)
         cv.waitKey(500)
 
 cv.destroyAllWindows()
 
 # Camera Calibration
-ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-
+height, width = gray.shape[:2]
+# Camera Calibration
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints,
+                                                  imgpoints,
+                                                  (width,
+                                                   height),
+                                                  None,
+                                                  None)
 # Refine Camera Matrix
 img = cv.imread(images[0])
-height, width = img.shape[:2]
 ref_cam_mtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (width, height), 1, (width, height))
 
 # Transform the matrix and distortion coefficients to writable lists
@@ -55,5 +84,8 @@ data = {'camera_matrix': np.asarray(ref_cam_mtx).tolist(),
         'tvecs': np.asarray(tvecs).tolist()}
 
 # Save calibration parameters to a yaml file
-with open("calibration_matrix.yaml", "w") as f:
+with open("calibration_matrix3.yaml", "w") as f:
     yaml.dump(data, f)
+
+view_err = ReprojectionError(imgpoints, objpoints, mtx, dist, rvecs, tvecs)
+print(f'Reprojection Err: {view_err}')
